2928a827a8a68dd4116ef8b4c958ba8b60cfef52
tahminahesari <hesari@live.com>
2021-10-05 20:54:50 +0200

    FetchData

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
 import './App.css';
+import axios from "axios";
+
+const apiKey =
+    '62780ad2de9b8538cfdd84ddaafdb93a';
 
 function App() {
+async function fetchData() {
+  try {
+    const result = await
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}`)
+    console.log(result.data);
+  }  catch (e) {
+          console.log(e);
+   }
+  }
+
   return (
-    <>
+          <>
       <div className="weather-container">
 
         {/*HEADER -------------------- */}
@@ -18,7 +32,9 @@ function App() {
             <h3> </h3>
             <h1>14 &deg;</h1>
 
-            <button type="button">
+            <button type="button"
+                onClick={fetchData}
+            >
               Haal data op!
             </button>
           </span>
